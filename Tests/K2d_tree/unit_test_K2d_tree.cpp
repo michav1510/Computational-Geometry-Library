f277c89ed1a90b399e091b1068a8201c7a5fc55b
Chaviaras Michalis <michav1510@gmail.com>
2018-07-11 14:18:59 +0300

    unit tests for K2d_tree

--- Tests/K2d_tree/unit_test_K2d_tree.cpp
+++ Tests/K2d_tree/unit_test_K2d_tree.cpp
@@ -14,10 +14,10 @@ int main(int argc, char* argv[])
 void test_parameter_constructor(std::ostream& write_str)
 {
 	write_str << "\n\nHere is checked the \n";
-	write_str << "Point2d() :\n";
+	write_str << "K2d_tree(std::vector<Point2d> points) :\n";
 	
 	int cas = 1;
-	while(cas <= 1)
+	while(cas <= 5)
 	{
 		if(cas == 1)
 		{
@@ -45,7 +45,24 @@ void test_parameter_constructor(std::ostream& write_str)
 							K2d_tree::tree_iterator it15 = it1.getLeftChild().getRightChild();
 							if(it15.getSplitCoord() == 0 && it15.getSplitValue() == 0 && it15.getLeafPoint() == Point2d(-1,2) && it15.isInternalNode() == false)
 							{
-								write_str << "sdfsdf\n";
+								
+								K2d_tree::tree_iterator it16 = it1.getRightChild().getLeftChild();
+								if(it16.getSplitCoord() == 0 && it16.getSplitValue() == 0 && it16.getLeafPoint() == Point2d(3,-5) && it16.isInternalNode() == false)
+								{
+									K2d_tree::tree_iterator it17 = it1.getRightChild().getRightChild();
+									if(it17.getSplitCoord() == 0 && it17.getSplitValue() == 0 && it17.getLeafPoint() == Point2d(4,-3) && it17.isInternalNode() == false)
+									{
+										K2d_tree::tree_iterator it18 = it1.getLeftChild().getLeftChild().getLeftChild();
+										if(it18.getSplitCoord() == 0 && it18.getSplitValue() == 0 && it18.getLeafPoint() == Point2d(1,0) && it18.isInternalNode() == false)
+										{
+											K2d_tree::tree_iterator it19 = it1.getLeftChild().getLeftChild().getRightChild();
+											if(it19.getSplitCoord() == 0 && it19.getSplitValue() == 0 && it19.getLeafPoint() == Point2d(2,1) && it19.isInternalNode() == false)
+											{
+												test_pass1 = true;
+											}
+										}
+									}
+								}
 							}						
 						}
 					}
